f65a8a6eb6f6f7a6cd357bf3e2a72fde29bff02a
azukisiromochi <azukisiromochi@gmail.com>
2020-04-03 22:55:29 +0900

    Update font usage 2

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,17 +4,27 @@
   </div>
 </template>
 
-<style>
+<script>
+export default {
+  head: {
+    link: [
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css?family=Baloo+Thambi+2&display=swap'
+      }
+    ]
+  },
+}
+</script>
+
+<style lang="scss">
 html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
   box-sizing: border-box;
+  body {
+    font-family: 'Baloo Thambi 2' !important;
+  }
 }
 
 *,
